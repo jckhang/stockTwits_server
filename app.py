08e2625b6356c267036351757cc4f987a704d0d9
Yuxiang Zhang <rico.zhang9318@gmail.com>
2016-04-05 18:21:19 -0400

    update

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 
 @app.route('/')
-def index():
-    return 'hello world'
+def hello():
+    return 'Hello world'
+
 if __name__ == '__main__':
-    app.debug = True
     app.run()
