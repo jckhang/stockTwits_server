44d20f389e2949d733f77b59805917b520d182c9
Yuxiang Zhang <rico.zhang9318@gmail.com>
2016-04-05 18:37:48 -0400

    update

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
+@app.route("/")
+def home():
+    return render_template("home.html", name="home")
 
-@app.route('/')
-def hello():
-    return 'Hello world'
-
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    app.run(debug=True)
