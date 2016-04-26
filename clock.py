9809974f7abd419764c619c8223e204f47f54759
Yuxiang Zhang <rico.zhang9318@gmail.com>
2016-04-26 18:33:34 -0400

    Update

--- clock.py
+++ clock.py
@@ -8,14 +8,16 @@ def timed_job():
     updateDBstock()
     print('stock price is updated every 10 minutes.')
 
+
 @sched.scheduled_job('interval', minutes=30)
 def timed_job():
     updateDBtwits()
     print('stock twits is updated every 30 minutes.')
 
+
 @sched.scheduled_job('cron', day_of_week='mon-fri', hour='9.5-16')
 def scheduled_job():
-	updateDBstock()
+    updateDBstock()
     print('This job is run every weekday at 9:30 to 16.')
 
 sched.start()
