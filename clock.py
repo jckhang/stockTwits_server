aa55fbf67898fd6e757cff157d5bcdf87c7dd80b
Yuxiang Zhang <rico.zhang9318@gmail.com>
2016-04-26 20:41:24 -0400

    Check if it works?

--- clock.py
+++ clock.py
@@ -4,13 +4,13 @@ sched = BlockingScheduler()
 
 
 @sched.scheduled_job('interval', minutes=10)
-def timed_job():
+def timed_job1():
     updateDBstock()
     print('stock price is updated every 10 minutes.')
 
 
 @sched.scheduled_job('interval', minutes=30)
-def timed_job():
+def timed_job2():
     updateDBtwits()
     print('stock twits is updated every 30 minutes.')
 
