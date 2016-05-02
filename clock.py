965c2f35d42f38011780a2491de7e7b3c74edae7
Yuxiang Zhang <rico.zhang9318@gmail.com>
2016-05-02 16:58:07 -0400

    Change back clock.py

--- clock.py
+++ clock.py
@@ -3,22 +3,22 @@ from app import updateDBstock, updateDBtwits
 sched = BlockingScheduler()
 
 
-@sched.scheduled_job('interval', minutes=10)
-def timed_job1():
-    updateDBstock()
-    print('stock price is updated every 10 minutes.')
+# @sched.scheduled_job('interval', minutes=10)
+# def timed_job1():
+#     updateDBstock()
+#     print('stock price is updated every 10 minutes.')
 
 
-@sched.scheduled_job('interval', minutes=30)
-def timed_job2():
-    updateDBtwits()
-    print('stock twits is updated every 30 minutes.')
+# @sched.scheduled_job('interval', minutes=30)
+# def timed_job2():
+#     updateDBtwits()
+#     print('stock twits is updated every 30 minutes.')
 
 
 @sched.scheduled_job('cron', day_of_week='mon-fri', hour='9.5-16')
 def scheduled_job():
-    timed_job1()
-    timed_job2()
+    updateDBstock()
+    updateDBtwits()
     print('This job is run every weekday at 9:30 to 16.')
 
 sched.start()
