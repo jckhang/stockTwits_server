eb4bb248b4410065cb0572aa6fa9b4b65a79e9eb
Yuxiang Zhang <rico.zhang9318@gmail.com>
2016-05-05 17:07:52 -0400

    Change clock.py

--- clock.py
+++ clock.py
@@ -19,7 +19,7 @@ def scheduled_job():
     print('This job is run every weekday at 9:30 to 16.')
 
 
-sched.add_job(scheduled_job, 'cron', day_of_week='mon-fri', hour='9.5-16')
+sched.add_job(scheduled_job, 'cron', day_of_week='mon-fri', hour='9-16')
 
 
 sched.start()
